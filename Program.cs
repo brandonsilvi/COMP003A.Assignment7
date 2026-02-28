85c65f5c58805bf138086a99f400ef8801fedae5
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-27 19:26:23 -0800

    menu loop for options

--- Program.cs
+++ Program.cs
@@ -14,6 +14,21 @@ namespace COMP003A.Assignment7
             List<int> scores = new List<int> { 80, 72, 95, 87, 79 };
 
             bool exitProgram = false;
+            
+            //menu loop
+            while (!exitProgram)
+            {
+                Console.WriteLine("\n1. Display all values");
+                Console.WriteLine("2. Show Total");
+                Console.WriteLine("3. Show Average");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter Choice: ");
+
+                string input = Console.ReadLine();
+                int choice = 0;
+                
+                
+            }
         }
     }
 }
