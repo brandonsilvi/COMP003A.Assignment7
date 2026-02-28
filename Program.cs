c264204c8e4c567d53dff8450041e8dce8afe2ef
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-27 19:11:32 -0800

    List made

--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@ namespace COMP003A.Assignment7
     class Program
     {
         static void Main(string[] args)
-        {
+        { //student sore analyzer
+            /* Program purpose is to store a collection of students scores 
+            allowing user to display values, calculate total, and calculate average*/
             
+            //list to store numeric values
+            List<int> scores = new List<int> { 80, 72, 95, 87, 79 };
+
+            bool exitProgram = false;
         }
     }
 }
